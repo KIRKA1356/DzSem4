93dbadc1def59334edf564308532a46c152c3ca2
KIRKA1356 <kgritsun@mail.ru>
2022-07-03 19:49:41 +0300

    Конец

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+/*
+Console.WriteLine("Введите число: ");                      //Задача 27
+int x = Convert.ToInt32(Console.ReadLine());
+
+double Sum(int x)
+{
+    double sum = 0;
+    String ch = Convert.ToString(x);
+    int count = ch.Length;
+    for (int i = 0; i<count; i++)
+        {
+            double s =  char.GetNumericValue(ch[i]);
+            sum = s + sum;
+        }
+    return sum;
+}
+Console.WriteLine("Cумма равна: " + Sum(x));
+*/
+
+
+
+
+
+
+int[] array = new int[8];                                   //Задача 29
+void masiv(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i<count; i++)
+        {
+            int x = new Random().Next(1,10);
+            array[i] = x;
+            Console.Write($"{array[i]} ");
+        }
+}
+masiv(array);
+
+
